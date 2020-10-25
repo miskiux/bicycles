71ae57568de2eaa083c270ecfd426ab4799acd31
gecasjus <70134764+gecasjus@users.noreply.github.com>
2020-10-25 23:55:37 +0200

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,16 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-    apiKey: "AIzaSyAPpbMBEihUbYOuO51R6f-Um88OZXo5I68",
-    authDomain: "bici-3be47.firebaseapp.com",
-    databaseURL: "https://bici-3be47.firebaseio.com",
-    projectId: "bici-3be47",
-    storageBucket: "bici-3be47.appspot.com",
-    messagingSenderId: "397239191781",
-    appId: "1:397239191781:web:5105e88a7099a054dcac0c",
-    measurementId: "G-436ZVS1NPV"
-  };
 
   firebase.initializeApp(config);
 
