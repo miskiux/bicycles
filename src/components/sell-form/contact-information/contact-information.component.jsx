d23f25caf2a516bf8ad983a1999347e67ca91761
Justas <Justas@Justass-MacBook-Air.local>
2021-01-07 13:47:31 +0200

    geocoder reverse

--- src/components/sell-form/contact-information/contact-information.component.jsx
+++ src/components/sell-form/contact-information/contact-information.component.jsx
@@ -9,11 +9,16 @@ import './contact-information.css'
 
 
 //get current location on Click
-// search input to close
+
+//toggle contribution triggering the form upload
+
 const mapStyle = {
     width: '70%',
     height: 600
-} 
+}
+
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding({ accessToken: process.env.REACT_APP_API_KEY}); 
 
 const ContactInformation = (props) => {
 
@@ -24,6 +29,8 @@ const ContactInformation = (props) => {
     bearing: 0,
     pitch: 0,
   });
+
+	const [address, setAddress] = useState([])
 
 	const mapRef = useRef();
 	
@@ -44,13 +51,23 @@ const ContactInformation = (props) => {
     [handleViewportChange]
   );
 
-  
- 
+  const reverseCoordinates = () => {
+  	geocodingClient.reverseGeocode({
+  	query: [viewport.longitude, viewport.latitude]
+})
+  .send()
+  .then(response => {
+    const match = response.body;
+    console.log(match);
+  });
+  }
+   
 	return (
 		<div>
 		{
 			props.currentStep == 2 ?
 			<div>
+			{console.log(viewport)}
 				<h2 className="bici-location">bicycle location</h2>
 				<div className="map">
 					<ReactMapGL
@@ -63,8 +80,9 @@ const ContactInformation = (props) => {
 				      <Geocoder
 				      	  className="react-geocoder"
 				          mapRef={mapRef}
-				          onViewportChange={handleGeocoderViewportChange}
-				     
+				          onViewportChange={handleGeocoderViewportChange} 
+				          reverseGeocode={true}
+				          localGeocoder={reverseCoordinates}    
 				          mapboxApiAccessToken={process.env.REACT_APP_API_KEY}
 				          position="top-left"
 				        />
